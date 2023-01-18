6acf914c319a7e6b07c8f4ed558dcdc9531ec21c
Lorean7 <oleglone7@gmail.com>
2023-01-18 10:10:34 +0300

    объединение фронта и бэка

---
+++ backend/pre-server-nodejs/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { PythonShell } from 'python-shell';
+import shell from 'shelljs';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// ?: Конфигруация
+let PORT = process.env.PORT || 3456;
+
+app.listen(PORT, () => {
+  console.log('server is running to port: ' + PORT);
+});
+
+app.get("/1", (req, res) => {
+  shell.exec('sh launch.sh', (error, stdout, stderr) => {
+    console.log(stdout);
+  })
+  res.send(stdout);
+})
--- backend/software/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 pool.py 1600000 2800000 2000 test.csv no-report z-only
